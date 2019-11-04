e15919da0ad0879a5e252942bb776e1748807875
Noel Light-Hilary <noel@noellh.com>
2019-11-04 13:35:32 +0000

    API-8 - apply basic reprocessing on save
    * Scale down very large images, maintaining aspect
    ratio but with a maximum largest dimension of
    2,500px
    * Compress images and reprocess, supporting JPEG and PNG

--- Readme.md
+++ Readme.md
@@ -3,7 +3,12 @@
 This is a Node.js project currently containing a single function for AWS Lambda in `index.js`,
 which provides the `POST` endpoint for the Big Give's user-generated content API.
 
-## Package for Lambda manually
+## Install packages and build Sharp for Lambda
+
+* `npm install`
+* Follow [instructions for Sharp on Lambda](https://sharp.pixelplumbing.com/en/stable/install/#aws-lambda) (Docker approach on Linux).
+
+## Upload Lambda package manually
 
 * Zip up the *contents* of the folder (not the whole folder), including `node_modules/`.
 * Upload [here](https://eu-west-2.console.aws.amazon.com/lambda/home?region=eu-west-2#/functions/content-api-post-sandbox?tab=graph).
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict'
 
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 const fileType = require('file-type');
+const sharp = require('sharp')
 const uuidv4 = require('uuid/v4');
-
-const s3 = new AWS.S3();
 
 function fail(message, code, callback) {
   if (code == 500) {
@@ -26,8 +25,7 @@ exports.handler = function (event, context, callback) {
   }
 
   const data = JSON.parse(event.body);
-  const encodedImage = data.body;
-  const decodedImage = Buffer.from(encodedImage, 'base64');
+  const decodedImage = Buffer.from(data.body, 'base64');
 
   if (!decodedImage || !data.accountId || !data.type) {
     return fail('Missing required metadata', 400, callback);
@@ -38,34 +36,55 @@ exports.handler = function (event, context, callback) {
     return fail('Unrecognised file type', 400, callback);
   }
 
-  const generatedName = `${uuidv4()}.${mimeType.ext}`;
+  const maxSize = 2500;
 
-  const path = `${data.accountId}/${data.type}/${generatedName}`;
-  const s3Params = {
-    ACL: 'public-read',
-    Body: decodedImage,
-    Bucket: process.env.S3_BUCKET,
-    ContentType: mimeType.mime,
-    Key: path,
-    Metadata: {
-      SalesforceAccountId: data.accountId,
-      SalesforceCCampaignId: data.ccampaignId ? data.ccampaignId : null,
-      SalesforceContentDocumentId: data.contentDocumentId ? data.contentDocumentId : null,
-      SalesforceContentType: data.contentType ? data.contentType : null,
-      SalesforceContentVersionId: data.contentVersionId ? data.contentVersionId : null,
-      SalesforceFilename: data.name ? data.name : null,
-      SalesforceUserId: data.userId ? data.userId : null,
-    }
-  };
+  sharp(decodedImage)
+    .resize({
+      fit: sharp.fit.inside,
+      withoutEnlargement: true,
+      width: maxSize,
+      height: maxSize,
+    })
+    .jpeg({ progressive: true, quality: 85, force: false })
+    .png( { progressive: true, compressionLevel: 9, adaptiveFiltering: true, force: false })
+    .withMetadata()
+    .toBuffer()
+    .then(processedImage => {
+      const generatedName = `${uuidv4()}.${mimeType.ext}`;
+      const path = `${data.accountId}/${data.type}/${generatedName}`;
+      const s3Params = {
+        ACL: 'public-read',
+        Body: processedImage,
+        Bucket: process.env.S3_BUCKET,
+        ContentType: mimeType.mime,
+        Key: path,
+        Metadata: {
+          SalesforceAccountId: data.accountId,
+          SalesforceCCampaignId: data.ccampaignId ? data.ccampaignId : null,
+          SalesforceContentDocumentId: data.contentDocumentId ? data.contentDocumentId : null,
+          SalesforceContentType: data.contentType ? data.contentType : null,
+          SalesforceContentVersionId: data.contentVersionId ? data.contentVersionId : null,
+          SalesforceFilename: data.name ? data.name : null,
+          SalesforceUserId: data.userId ? data.userId : null,
+        }
+      };
 
-  s3.putObject(s3Params, function (error) {
-    if (error) {
-      fail('Save error: ' + error, 500, callback);
+      const s3 = new AWS.S3({signatureVersion: 'v4'});
+      s3.putObject(s3Params, function (error) {
+        if (error) {
+          fail('Save error: ' + error, 500, callback);
+          return;
+        }
+    
+        callback(null, {"statusCode": 200, "body": JSON.stringify({
+          // TODO generalise base image handling so it looks like this
+          // 'uri': `${process.env.IMAGE_ACCESS_BASE_URI}/${path}`,
+          'uri': `https://${process.env.S3_BUCKET}.s3.eu-west-2.amazonaws.com/${path}`,
+        })});
+      });
+    })
+    .catch(sharpError => {
+      fail('Processing error: ' + sharpError, 500, callback);
       return;
-    }
-
-    callback(null, {"statusCode": 200, "body": JSON.stringify({
-      'uri': `https://${process.env.S3_BUCKET}.s3.eu-west-2.amazonaws.com/${path}`,
-    })});
-  });
+    })
 }
